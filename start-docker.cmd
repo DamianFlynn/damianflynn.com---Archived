a2f51251b0bd608ca16562bb4608f1f2eb335dda
Damian Flynn <info@damianflynn.com>
2017-09-04 09:57:34 +0100

    updating gitIgnore file, updating docker Setting Docker to use the Production environment variables to check site.url mapping

--- start-docker.cmd
+++ start-docker.cmd
@@ -1,2 +1,2 @@
-docker run --rm -it -p 4000:4000 -v c:/cloud/Dropbox/code/blog/damianflynn.com:/srv/jekyll jekyll/jekyll:pages jekyll serve --incremental
+docker run --rm -it -p 4000:4000 -v c:/cloud/Dropbox/code/blog/damianflynn.com:/srv/jekyll -e "JEKYLL_ENV=production" jekyll/jekyll:pages jekyll serve --incremental
 
