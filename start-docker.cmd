f7a313b1cea73ed56fe06be237c9d711e516fc47
Damian Flynn (MVP) <damian.flynn@lumagate.com>
2018-04-05 08:25:57 +0100

    Update to the Docker CLI

--- start-docker.cmd
+++ start-docker.cmd
@@ -1,2 +1,2 @@
-docker run --rm -it -p 4000:4000 -v c:/cloud/Dropbox/code/blog/damianflynn.com:/srv/jekyll -e "JEKYLL_ENV=production" jekyll/jekyll:pages jekyll serve --incremental
+docker run --rm -it -p 4000:4000 -v ${PWD}:/srv/jekyll -e "JEKYLL_ENV=production" jekyll/jekyll bash -c "bundle install && jekyll serve --incremental --watch --force_polling"
 
